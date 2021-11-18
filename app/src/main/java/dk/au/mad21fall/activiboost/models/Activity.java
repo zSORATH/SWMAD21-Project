8ff1949653da00105e9d88cd407b051158e1dcf5
Karen <karen.bjerringfelt@gmail.com>
2021-11-18 15:42:03 +0100

    activity

--- app/src/main/java/dk/au/mad21fall/activiboost/models/Activity.java
+++ app/src/main/java/dk/au/mad21fall/activiboost/models/Activity.java
@@ -8,6 +8,7 @@ public class Activity {
     private String activityName;
     private Date time;
     private ArrayList<Patient> participants;
+    private ArrayList<Caregiver> caregivers;
 
     public Activity(){}
 
@@ -34,4 +35,13 @@ public class Activity {
     public void setParticipants(ArrayList<Patient> participants) {
         this.participants = participants;
     }
+
+    public ArrayList<Caregiver> getCaregivers() {
+        return caregivers;
+    }
+
+    public void setCaregivers(ArrayList<Caregiver> caregivers) {
+        this.caregivers = caregivers;
+    }
+
 }
