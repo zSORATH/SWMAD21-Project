b85d5ef80e7a4864f09fdff9adc5fb28b537fe0b
Karen <karen.bjerringfelt@gmail.com>
2021-11-18 15:16:20 +0100

    activity

--- app/src/main/java/dk/au/mad21fall/activiboost/Model/Activity.java
+++ app/src/main/java/dk/au/mad21fall/activiboost/Model/Activity.java
@@ -1,9 +1,37 @@
 package dk.au.mad21fall.activiboost.Model;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 public class Activity {
 
     private String activityName;
-    private Date date;
+    private Date time;
+    private ArrayList<Patient> participants;
+
+    public Activity(){}
+
+    public String getActivityName() {
+        return activityName;
+    }
+
+    public void setActivityName(String activityName) {
+        this.activityName = activityName;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public ArrayList<Patient> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(ArrayList<Patient> participants) {
+        this.participants = participants;
+    }
 }
