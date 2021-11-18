efcd7528ed4595146428602b2a2c0f313dd361ce
Jonas Lund <1709-au650143@users.noreply.gitlab.au.dk>
2021-11-18 20:31:17 +0100

    now it should be good (bottom navigation and temp login)

---
+++ app/src/main/java/dk/au/mad21fall/activiboost/database/DiaryDatabase.java
@@ -0,0 +1,29 @@
+package dk.au.mad21fall.activiboost.database;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+// This is from the "Code Demo / walkthrough : using Room (and SharedPreferences)" from lecture 4
+@Database(entities = {Diary.class}, version = 4)
+public abstract class DiaryDatabase extends RoomDatabase{
+
+    public abstract DiaryDAO diaryDAO();  //mandatory DAO getter
+    private static DiaryDatabase instance; //database instance for singleton
+
+    public static DiaryDatabase getDatabase(final Context context) {
+        if (instance == null) {
+            synchronized (DiaryDatabase.class) {
+                if (instance == null) {
+                    instance = Room.databaseBuilder(context.getApplicationContext(),
+                            DiaryDatabase.class, "diary_database")
+                            .fallbackToDestructiveMigration()
+                            .build();
+                }
+            }
+        }
+        return instance;
+    }
+}
