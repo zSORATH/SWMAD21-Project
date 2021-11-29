9bd4e5815570229e7e9615ff572996da39a4a6bb
zSORATH <pernillepallesen3@gmail.com>
2021-11-29 16:13:47 +0100

    changed age to string

--- app/src/main/java/dk/au/mad21fall/activiboost/models/Patient.java
+++ app/src/main/java/dk/au/mad21fall/activiboost/models/Patient.java
@@ -12,7 +12,7 @@ public class Patient {
     private int pid;
 
     private String name;
-    private int age;
+    private String age;
     private String id;
 
     public Patient(){}
@@ -25,11 +25,11 @@ public class Patient {
         this.name = name;
     }
 
-    public int getAge() {
+    public String getAge() {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(String age) {
         this.age = age;
     }
 
