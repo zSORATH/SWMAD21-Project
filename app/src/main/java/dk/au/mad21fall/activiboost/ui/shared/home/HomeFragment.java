013f79b59932b74ef868cd5d993336b54cdc11c9
Mie Nielsen <mie.240892@gmail.com>
2021-12-05 02:47:07 +0100

    ADD START: Checking for Todays activities

--- app/src/main/java/dk/au/mad21fall/activiboost/ui/shared/home/HomeFragment.java
+++ app/src/main/java/dk/au/mad21fall/activiboost/ui/shared/home/HomeFragment.java
@@ -2,27 +2,39 @@ package dk.au.mad21fall.activiboost.ui.shared.home;
 
 import static dk.au.mad21fall.activiboost.Constants.PATIENT;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
 import dk.au.mad21fall.activiboost.Constants;
+import dk.au.mad21fall.activiboost.R;
 import dk.au.mad21fall.activiboost.databinding.FragmentHomeBinding;
+import dk.au.mad21fall.activiboost.models.Activity;
 import dk.au.mad21fall.activiboost.services.LocationUtil;
 import dk.au.mad21fall.activiboost.services.WeatherApi;
+import dk.au.mad21fall.activiboost.ui.shared.calendar.CalendarViewModel;
+import sun.bob.mcalendarview.vo.DateData;
 
 public class HomeFragment extends Fragment {
 
@@ -35,6 +47,14 @@ public class HomeFragment extends Fragment {
     private LocationUtil locationUtil = new LocationUtil();
     private String location;
 
+    private Activity currentActivity;
+
+
+    private MutableLiveData<ArrayList<Activity>> mActivities = new MutableLiveData<>();
+    private ArrayList<Activity> activities = new ArrayList<>();
+
+    private CalendarViewModel cvm;
+
     WeatherApi api = new WeatherApi();
     private FusedLocationProviderClient fusedLocationClient;
 
@@ -44,6 +64,8 @@ public class HomeFragment extends Fragment {
         super.onCreate(savedInstanceState);
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(getContext());
 
+
+
         getWeather();
 
     }
@@ -51,6 +73,18 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         hmv = new ViewModelProvider(this).get(HomeViewModel.class);
+        cvm = new ViewModelProvider(this).get(CalendarViewModel.class);
+
+
+        if(getCalendar() == true){
+            Toast.makeText(getContext(), "You have an activity today", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "You have an activity today");
+            // TODO: Show activity
+        } else {
+            Toast.makeText(getContext(),  "No activities today", Toast.LENGTH_SHORT).show();
+            Log.d(TAG, "No activities today");
+            // TODO: Give no activity message in view
+        }
 
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
@@ -69,10 +103,27 @@ public class HomeFragment extends Fragment {
         return root;
     }
 
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
+    private boolean getCalendar() {
+        mActivities = cvm.getActivities(uid);
+        activities = mActivities.getValue();
+        Calendar calendar = Calendar.getInstance();
+        String today = ""+ calendar.get(Calendar.YEAR) + calendar.get(Calendar.MONTH) + calendar.get(Calendar.DAY_OF_MONTH);;
+        String _aDate;
+
+
+        Log.d(TAG, "Today is: " + today);
+
+        for (Activity a : activities) {
+
+            _aDate = "" + calendar.get(Calendar.YEAR) + (calendar.get(Calendar.MONTH)+1) + calendar.get(Calendar.DAY_OF_MONTH);
+            if (today.equals(_aDate)) {
+                Log.d(TAG, "Activity found: " + a.getActivityName());
+                currentActivity = a;
+                return true;
+            }
+        }
+        return false;
+
     }
 
     private void getWeather() {
@@ -85,5 +136,11 @@ public class HomeFragment extends Fragment {
          String testLocation =  "lat=56.16216216216216&lon=10.160215237798806";
          api.getLocalWeather(testLocation, context);
 
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
